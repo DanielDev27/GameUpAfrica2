364520eebd34b911beb7d399e235407868b83cba
DanielDev27 <danielbeale27@gmail.com>
2024-09-06 09:27:11 +0200

    Moved completed project to repo
    Accidently completed the project task before making a repo

---
+++ GUAChallenge2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    [SerializeField] private float cooldown = 0.5f;
+    private float timer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && timer >= cooldown)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timer = 0.0f;
+        }
+        if (timer < cooldown)
+        {
+            timer += Time.deltaTime;
+        }
+    }
+}
